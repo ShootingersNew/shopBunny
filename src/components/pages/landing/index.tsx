8a76f40f9392b6da0c7d70bb132f9871cac02991
shootingersNew <lebedevseergey@gmail.com>
2020-07-26 18:56:05 +0300

    headline and scrolling to anchor have been added

--- src/components/pages/landing/index.tsx
+++ src/components/pages/landing/index.tsx
@@ -1,18 +1,34 @@
-import React from "react";
+import React, {useEffect, useRef, useState} from "react";
 import Index from "../../templates/landingTemplate";
 import Header from "../../organisms/header";
 import Headline from "../../organisms/headline";
+import back from './img/back.jpg'
+
+type ScrollRefsTypes = {
+    projects: number | undefined
+};
 
 const Landing = () => {
+    //initialize state for store offsetTop of scroll-wrappers
+    const [scrollRefs, setScrollRefs] = useState<ScrollRefsTypes>({projects: undefined});
+    const projectsRef = useRef<HTMLDivElement>(null);
+    const useMountEffect = (fun: () => void) => useEffect(fun, []);
+    const initRefs = (): void => {
+        const projectsOffset: number | undefined = projectsRef.current?.offsetTop;
+        setScrollRefs({...scrollRefs, projects: projectsOffset})
+    };
+    useMountEffect(initRefs);
     return (
         <Index
-            header={<Header/>}
+            header={<Header scrollTo={scrollRefs.projects}/>}
             footer={<footer>eee</footer>}
             headline={
-                <Headline/>
+                <Headline img={back} linkTo={scrollRefs.projects}/>
             }
         >
-
+            <div ref={projectsRef} id="anchor" className="element">
+                test 6 (anchor)
+            </div>
         </Index>
     )
 
