b359cc1ab128bfb37824222735723e2883029d0b
shootingersNew <lebedevseergey@gmail.com>
2020-07-26 19:02:15 +0300

    scrollLink has been created

---
+++ src/components/molecules/scrollLink/index.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+import './style.css'
+import cn from "classnames";
+import s from './../../atoms/mainColor/style.module.css'
+import f from '../../atoms/montserratBold/style.module.css'
+
+interface ScrollLinkPropTypes {
+    className?: string,
+    children: string,
+    to: number | undefined
+}
+
+const ScrollLink: React.FC<ScrollLinkPropTypes> = ({children, className, to}) => {
+    let classNames = cn({
+        'scrollLink': true,
+        [`${className}`]: className
+
+    });
+    const clickHandler = () => {
+        scrollToRef(to)
+    };
+    const scrollToRef = (to: number | undefined): void => {
+        const BodySelector: HTMLBodyElement | null = document.querySelector('body');
+        if (BodySelector !== null) {
+            BodySelector.scrollTo({
+                top: to,
+                behavior: "smooth"
+            })
+        }
+    };
+    return (
+        <a className={classNames + ' ' + s.mainColor + ' ' + f.montserratBold} onClick={clickHandler}>
+            {children}
+        </a>
+    )
+};
+export default ScrollLink
