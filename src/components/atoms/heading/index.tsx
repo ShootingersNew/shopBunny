7df27538b5e48797bbc2e17cb037f08a2efabd5f
shootingersNew <lebedevseergey@gmail.com>
2020-07-26 18:54:39 +0300

    heading has been added

---
+++ src/components/atoms/heading/index.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import cn from 'classnames'
+import './style.css'
+import f from './../montserratBlack/style.module.css'
+
+type HeadingPropTypes = {
+    type: number,
+    className?: string
+};
+
+const Heading: React.FC<HeadingPropTypes> = ({type, className, children}) => {
+    let headingClassname = cn({
+        [`heading_${type} ${f.fontMontserratBlack}`]: true,
+        [`${className}`]: className,
+    });
+    let h;
+    switch (type) {
+        case 1:
+            h = <h1 className={headingClassname}>{children}</h1>;
+            break;
+        case 2:
+            h = <h2 className={headingClassname}>{children}</h2>;
+            break;
+        case 3:
+            h = <h3 className={headingClassname}>{children}</h3>;
+            break;
+        default :
+            h = <h4 className={headingClassname}>{children}</h4>
+    }
+    return h
+};
+export default Heading
