c606b25d861c98cc10251a05e6ce94608675595f
shootingersNew <lebedevseergey@gmail.com>
2020-08-18 13:47:26 +0300

    add card ts types

--- src/tsTypes.tsx
+++ src/tsTypes.tsx
@@ -4,3 +4,22 @@ export type ProductArrayType = {
     type: string,
     tags: string[]
 }
+export type StaffArrayType = {
+    preview: string,
+    name: string,
+    post: string
+}
+export type InfoArrayType = {
+    title: string,
+    price: string,
+    shortDesc: string,
+    definition: string,
+    purpose: string[]
+}
+export type PriceArrayType = {
+    preview: string
+    title: string[] | string,
+    subTitle: string | JSX.Element | object,
+    type: string,
+    list: string[]
+}
