34bdb72709d3c4f0c6cc38c9ba2431ed9768441b
shootingersNew <lebedevseergey@gmail.com>
2020-09-16 14:21:16 +0300

    change span to input

--- src/components/atoms/Tag/index.tsx
+++ src/components/atoms/Tag/index.tsx
@@ -1,31 +1,56 @@
-import React from "react";
+import React from "react"
+import Input from "../Input"
 import cn from 'classnames'
 import './style.css'
 
-const Tag: React.FC<{ filterClickHandler?: (val: string) => void, className?: string, link?: string }> =
-    ({filterClickHandler, children, link, className}) => {
+type TagPropTypes = {
+    filterClickHandler?: (val: string) => void,
+    className?: string,
+    link?: string,
+    name?: string,
+    defaultChecked?: boolean,
+    type?: 'radio' | 'checkbox'
+}
+const Tag: React.FC<TagPropTypes> =
+    ({
+         filterClickHandler,
+         children,
+         link,
+         className,
+         name = 'default',
+         defaultChecked = false,
+         type = 'radio'
+     }) => {
         const classNames = cn({
             tag: true,
             [`${className}`]: className
         });
-        const clickHandler: (e: React.MouseEvent) => void = (e) => {
-            const filter = (e.target as HTMLElement).getAttribute('data-tag');
+        const clickHandler: (e: React.FormEvent) => void = (e) => {
+            const filter = (e.target as HTMLElement).getAttribute('data-id');
             if (filterClickHandler) {
                 filterClickHandler(filter as string)
             }
         };
-
         return (
             <React.Fragment>
                 {
                     link ?
-                        <span onClick={clickHandler} data-id={link} data-tag={children} className={classNames}>
-                        #{children}
-                    </span>
+                        <label>
+                            <Input
+                                type={type}
+                                onChange={clickHandler}
+                                name={name}
+                                id={link}
+                                data-tag={children}
+                                className={classNames}
+                                defaultChecked={defaultChecked}
+                                defaultValue={children as string}
+                            />
+                            #{children}
+                        </label>
                         :
-                        <span onClick={clickHandler} className={classNames}>
-                        #{children}
-                    </span>
+                        <Input defaultChecked={defaultChecked} className={className} name={name} type={type}
+                               onChange={clickHandler}>#{children}</Input>
                 }
             </React.Fragment>
         )
