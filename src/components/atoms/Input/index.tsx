96ddbbc219b320c705e3dd843b38848d450c76e7
shootingersNew <lebedevseergey@gmail.com>
2020-09-16 14:15:23 +0300

    add input type checkbox & radio

--- src/components/atoms/Input/index.tsx
+++ src/components/atoms/Input/index.tsx
@@ -7,16 +7,18 @@ import fm from '../../atoms/montserratMedium/style.module.css'
 
 type InputTypes = {
     className?: string,
-    onChange?: () => void,
+    onChange?: (e: React.FormEvent) => void,
     value?: string,
     placeholder?: string,
     name: string,
     isValid?: void,
     register?: any,
     defaultValue?: string,
-    type?: 'textarea' | 'search'
+    type?: 'textarea' | 'search' | 'radio' | 'checkbox',
+    defaultChecked?: boolean,
+    id?: string
 };
-const Input: React.FC<InputTypes> = ({name, className, placeholder, register, isValid, defaultValue, type}) => {
+const Input: React.FC<InputTypes> = ({children, id, name, className, placeholder, register, isValid, defaultValue, type, defaultChecked}) => {
     let inputClass = classNames({
         'input': true,
         'input_isValid_true': isValid,
@@ -35,6 +37,36 @@ const Input: React.FC<InputTypes> = ({name, className, placeholder, register, is
                         defaultValue={defaultValue ? defaultValue : undefined}
                     />;
                 </span>;
+            break;
+        case "radio":
+            input =
+                <div className={inputClass + ' ' + className}>
+                    <input
+                        name={name}
+                        type='radio'
+                        className={'input__radio'}
+                        defaultChecked={defaultChecked}
+                        ref={register ? register : null}
+                        value={defaultValue}
+                        data-id={id}
+                    />
+                    {children}
+                </div>;
+            break;
+        case "checkbox":
+            input =
+                <div className={inputClass + ' ' + className}>
+                    <input
+                        name={name}
+                        type='checkbox'
+                        className={'input__checkbox'}
+                        defaultChecked={defaultChecked}
+                        ref={register ? register : null}
+                        value={defaultValue}
+                        data-id={id}
+                    />
+                    {children}
+                </div>;
             break;
         case "search":
             input =
