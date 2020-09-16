b8dcae01332f9126bb7a4d08451dc25a111b1202
shootingersNew <lebedevseergey@gmail.com>
2020-09-16 14:22:09 +0300

    import header & footer as default

--- src/components/templates/default/index.tsx
+++ src/components/templates/default/index.tsx
@@ -1,25 +1,25 @@
 import React from "react"
 import './styles.css'
 import Container from "../../atoms/Container"
+import Header from "../../organisms/Header";
+import Footer from "../../organisms/Footer";
 
 interface DefaultTemplateProps {
-    header: JSX.Element,
-    footer: JSX.Element,
     breadcrumbs: JSX.Element
 }
 
 const DefaultTemplate: React.FC<DefaultTemplateProps> =
-    ({header, footer, breadcrumbs, children}) => {
+    ({breadcrumbs, children}) => {
         return (
             <React.Fragment>
-                {header}
+                <Header scrollTo={111}/>
                 <main className="main">
                     <Container>
                         {breadcrumbs}
                         {children}
                     </Container>
                 </main>
-                {footer}
+                <Footer/>
             </React.Fragment>
 
         )
